149d86f94fbb65f4579627d3371ee28b4be46fe9
sekelsta <sekelsta@protonmail.com>
2024-04-27 08:33:13 -0400

    Male and female piglets, lambs, and chicks

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
+cp assets/vintage_inheritance/lang/es-419.json assets/vintage_inheritance/lang/es-es.json
+cp assets/genelib/lang/es-419.json assets/genelib/lang/es-es.json
 dotnet run --project ./Build/CakeBuild/CakeBuild.csproj -- "$@"
+rm assets/vintage_inheritance/lang/es-es.json
+rm assets/genelib/lang/es-es.json
 rm -r bin/
 rm -r src/obj/
 rm "$VINTAGE_STORY"/Mods/vintage_inheritance_*.zip
