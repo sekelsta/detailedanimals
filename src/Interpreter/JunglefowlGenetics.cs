5262d5ee50c610658f73a31dd2c1f826cbd48390
sekelsta <sekelsta@protonmail.com>
2024-09-13 13:49:53 -0500

    Start on chicken color genetics, but not hooked up to anything yet

--- src/Interpreter/JunglefowlGenetics.cs
+++ src/Interpreter/JunglefowlGenetics.cs
@@ -5,7 +5,28 @@ using Vintagestory.API.Common.Entities;
 namespace TruthBeauty {
     public class JunglefowlGenetics : GeneInterpreter {
         void GeneInterpreter.Interpret(Genome genome, Entity entity) {
-            // TODO
+            entity.WatchedAttributes.SetInt("textureIndex", getTextureIndex(genome));
+        }
+
+        private static int getTextureIndex(Genome genome) {
+            int duckwing = 0;
+            int black = 1;
+            int white = 2;
+            int blue = 3;
+            int bluesplash = 4;
+            if (genome.Homozygous("tyrosinase", "white")) {
+                return white;
+            }
+            if (genome.HasAutosomal("extension", "black")) {
+                if (genome.Homozygous("bluesplash", "bluesplash")) {
+                    return bluesplash;
+                }
+                else if (genome.HasAutosomal("bluesplash", "bluesplash")) {
+                    return blue;
+                }
+                return black;
+            }
+            return duckwing;
         }
     }
 }
