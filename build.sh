dbd8d2fd4ba04a69d62d0cff62d4bc39ab52dada
sekelsta <sekelsta@protonmail.com>
2024-12-19 20:48:59 -0600

    Warn if shapes are using null texture

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 # To define the environment variable, put something like this in your .bashrc file:
 # export VINTAGE_STORY_DEV="$HOME/software/vintagestory_dev"
 
+null_textured_shapes=$(grep -rl "#null" assets/)
+# Only print anything if files were found
+if [[ -n $null_textured_shapes ]]; then
+    echo "These shape files contain null textures:"
+    echo "$null_textured_shapes"
+fi
+
 python3 texsrc/cook.py
 cp assets/detailedanimals/lang/es-419.json assets/detailedanimals/lang/es-es.json
 cp assets/genelib/lang/es-419.json assets/genelib/lang/es-es.json
@@ -11,4 +18,3 @@ rm -r bin/
 rm -r src/obj/
 rm "${VINTAGE_STORY_DEV}"/Mods/detailedanimals_*.zip
 cp Build/Releases/detailedanimals_*.zip "${VINTAGE_STORY_DEV}/Mods"
-
