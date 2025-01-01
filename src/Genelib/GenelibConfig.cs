9f56bf309c46caaf5216991d31fae994cf7aed11
sekelsta <sekelsta@protonmail.com>
2025-01-01 07:54:12 -0600

    Add config for meat drops

--- src/Genelib/GenelibConfig.cs
+++ src/Genelib/GenelibConfig.cs
@@ -4,10 +4,15 @@ namespace Genelib {
     public class GenelibConfig {
         public string Units = "CUSTOMARY_METRIC";
 
+        public float AnimalMeat = 0.25f;
+
         public float InbreedingResistance = 0.5f;
+
+        public int ConfigVersion = 0;
 
         public void MakeValid() {
             InbreedingResistance = Math.Clamp(InbreedingResistance, 0.05f, 0.9f);
+            AnimalMeat = Math.Clamp(AnimalMeat, 0.01f, 100f);
         }
 
         public string WeightSuffix() {
@@ -21,6 +26,10 @@ namespace Genelib {
                 return "_kg_lbs";
             }
             return "_lbs_kg";
+        }
+
+        public float MeatMultiplier() {
+            return 4 * AnimalMeat;
         }
     }
 }
