8fd529238cee5e2d0f80a5131713cd94e4442cee
sekelsta <sekelsta@protonmail.com>
2025-01-23 18:44:11 -0600

    Time how long the build takes

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 # To define the environment variable, put something like this in your .bashrc file:
 # export VINTAGE_STORY_DEV="$HOME/software/vintagestory_dev"
 
+starttime=$(($(date +%s%N)/1000000))
+
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
@@ -11,7 +13,10 @@ if [[ -n $null_textured_shapes ]]; then
     echo -e "${null_textured_shapes}${NC}"
 fi
 
+precook=$(($(date +%s%N)/1000000))
 python3 texsrc/cook.py
+postcook=$(($(date +%s%N)/1000000))
+
 cp assets/detailedanimals/lang/es-419.json assets/detailedanimals/lang/es-es.json
 cp assets/genelib/lang/es-419.json assets/genelib/lang/es-es.json
 dotnet run --project ./Build/CakeBuild/CakeBuild.csproj -- "$@"
@@ -21,3 +26,9 @@ rm -r bin/
 rm -r src/obj/
 rm "${VINTAGE_STORY_DEV}"/Mods/detailedanimals_*.zip
 cp Build/Releases/detailedanimals_*.zip "${VINTAGE_STORY_DEV}/Mods"
+
+endtime=$(($(date +%s%N)/1000000))
+cooktime=$(( postcook - precook ))
+buildtime=$(( endtime - postcook ))
+totaltime=$(( endtime - starttime ))
+echo -e "${totaltime} milliseconds total: $((precook - starttime)) to validate, ${cooktime} to cook, and ${buildtime} to build"
